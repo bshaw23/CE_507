01bb27e051cdb024526b84ab33c09a40f6bfcf8a
bshaw23 <bshaw23@byu.edu>
2022-09-29 14:54:32 -0600

    2.1

--- Basis_Funct.py
+++ Basis_Funct.py
@@ -8,6 +8,12 @@ Created on Mon Sep 26 11:13:11 2022
 import sympy
 import matplotlib.pyplot as plt
 import numpy
+import scipy
+# import scipy.integrate
+from scipy import integrate
+import math
+
+print( __name__ )
 
 # 29
 def taylorExpansion( fun, a, order ):
@@ -22,21 +28,75 @@ def taylorExpansion( fun, a, order ):
         t += p
     return t
 
-#30
+#31
+x = sympy.Symbol('x')
 
 
+
+y1 = sympy.integrate(sympy.sin(sympy.pi*x))
+
+y2 = sympy.integrate(sympy.exp(x))
+
+y3 = sympy.integrate(sympy.erfc(x))
+
+print(y1)
+print(y2)
+print(y3)
+
+t1 = sympy.integrate(taylorExpansion(sympy.sin(sympy.pi*x),0,10))
+
+t2 = sympy.integrate(taylorExpansion(sympy.exp(x),0,10))
+
+t3 = sympy.integrate(taylorExpansion(sympy.erfc(x), 0, 10))
+
+print(t1)
+print(t2)
+print(t3)
+
+#32
+sin = sympy.sin(sympy.pi*x)
+e = sympy.exp(x)
+erfc = sympy.erfc(x)
+
+isin = []
+order = []
+for i in range (0,10):
+    err_fun = sympy.lambdify( x, abs( sin - taylorExpansion(sin, 0, i) ) )
+    isin.append( scipy.integrate.quad( err_fun, -1, 1 )[0] )
+    order.append(i)
+
+
+plt.plot( order, isin )
+plt.yscale('log')
+plt.show()
+
+# sympy.plot(isin, show = True, yscale = 'logrithmic', xlim = [-1,1])
+
+# isin = scipy.integrate.quad( err_fun, -1, 1 )[0]
+
+# print( isin )
+# ie = scipy.integrate(abs(e-taylorExpansion(e, 0, 10)))
+# ierfc = scipy.integrate(abs(erfc-taylorExpansion(erfc, 0, 10)))
+
+# sympy.plot(isin, show = True, yscale = 'logrithmic', xlim = [-1,1])
+# sympy.plot(ie, show = True, yscale = 'logrithmic', xlim = [-1,1])
+# sympy.plot(ierfc, show = True, yscale = 'logrithmic', xlim = [-2,2])
 
 
 
 #34
 
+## EQUIVALENT
+sin_2pi = lambda x : math.sin( 2*math.pi * x )
+
+def sin_2pi( x ):
+   return math.sin( 2 * math.pi * x) 
+## END EQUIVALENT
+    
 def evaluateMonomialBasis1D(p):
     x = sympy.Symbol('x')
     y = x**p
-    fig, ax = plt.subplots()
-    ax.plot(x,y)
-    plt.show()
-    return
+    return y
 
 import unittest
 
@@ -52,7 +112,7 @@ class Test_evaluateMonomialBasis1D( unittest.TestCase ):
            self.assertAlmostEqual( first = evaluateMonomialBasis1D( degree = p, variate = 0.5 ), second = 1 / ( 2**p ), delta = 1e-12 )
         
 #33
-# evaluateMonomialBasis1D(0)
+#sympy.plot(evaluateMonomialBasis1D(0), xlim=[-1,1])
 # evaluateMonomialBasis1D(1)
 # evaluateMonomialBasis1D(2)
 # evaluateMonomialBasis1D(3)
--- Chpt_2.1_Help.py
+++ Chpt_2.1_Help.py
@@ -12,12 +12,14 @@ import matplotlib.pyplot as plt
 
 x = sympy.Symbol('x')
 
-fun = sympy.sin(sympy.pi*x)
+#30
+fun = sympy.erfc(x)
 #Tx = BF.taylorExpansion(fun, 0, 10)
 #print(Tx)
 
-sympy.plot(sympy.sin(sympy.pi*x), BF.taylorExpansion(fun, 0, 0), BF.taylorExpansion(fun, 0, 1), \
-           BF.taylorExpansion(fun, 0, 3), BF.taylorExpansion(fun, 0, 5), \
-           BF.taylorExpansion(fun, 0, 7), show = True, xlim=[-1,1], ylim=[-2,2])
+
+sympy.plot(sympy.sin(sympy.erfc(x)), BF.taylorExpansion(fun, 0, 0), BF.taylorExpansion(fun, 0, 1), \
+           BF.taylorExpansion(fun, 0, 2), BF.taylorExpansion(fun, 0, 3), \
+           BF.taylorExpansion(fun, 0, 4), show = True, xlim=[-1,1], ylim=[0,3])
     
     
